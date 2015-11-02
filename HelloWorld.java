3a8ffde3a4412dc5ae6a5c59516894463aff36d1
bwalcott12 <bwalcott12@georgefox.edu>
2015-11-02 01:13:59 -0800

    Update HelloWorld.java
    Add block comment in top section of file.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,9 @@
+/*
+Prints "Hello, World". By tradition, this is everyone's first program.
+
+This is the first Java file that everyone will see, woot!
+*/
+
 public class HelloWorld {
 
     public static void main(String[] args) {
