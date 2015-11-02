98aad2dcc4e5c49fe2c438a82008d286f18e2f34
Brandon Walcott <bwalcott12@users.noreply.github.com>
2015-11-02 01:15:48 -0800

    Update HelloWorld.java
    Added little poem

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 /*
 Prints "Hello, World". By tradition, this is everyone's first program.
-
 This is the first Java file that everyone will see, woot!
+
+Roses are red, violets are blue...
+Coding is cool, so are cats... mic drop
 */
 
 public class HelloWorld {
