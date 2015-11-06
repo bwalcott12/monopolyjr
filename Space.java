053f774fdb401001a734402dad9ab45444cfd65c
McKenna Tornblad <mtornblad14@georgefox.edu>
2015-11-06 12:30:44 -0800

    added Space method

--- Space.java
+++ Space.java
@@ -5,6 +5,21 @@
  * @author Chase
  *
  */
-public class Space {
+public class Space 
+{
+	private String _name;
+	private int _location;
+	
+	private int Space(String name)
+	{
+		int location = 0;
+		return location;
+	}
+	
+	
+	private void performRole(Player player)
+	{
+		
+	}
 
 }
