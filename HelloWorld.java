1ea2aa3803d6f1dfbbf6da14fda88453dc71d9ea
Chase <Chase@Chase-PC>
2015-11-13 12:09:17 -0800

    c

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 Prints "Hello, World". By tradition, this is everyone's first program.
 This is the first Java file that everyone will see, woot!
@@ -11,6 +14,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        
+        Map<Integer, Space> spaces = new HashMap<Integer, Space>();
+        spaces.put(1, new Space("1", 1));
+        
+        
+        Board theBoard = new Board();
+        
     }
 }
 // eof
